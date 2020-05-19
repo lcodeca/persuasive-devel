179deebaa4b5f9542e721e95146a95134df36ab8
Lara CODECA <lara.codeca@gmail.com>
2020-05-19 13:09:42 +0100

    Gallery of PNG generation

---
+++ src/utils/HTMLGraphs/gallery.generator.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+
+""" Process the graph directory structure generating a static HTML gallery. """
+
+import argparse
+from collections import defaultdict
+import cProfile
+import io
+import json
+import logging
+import os
+from pprint import pformat, pprint
+import pstats
+import re
+import sys
+
+from jinja2 import Template
+
+from deepdiff import DeepDiff
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+
+def _argument_parser():
+    """ Argument parser for the stats parser. """
+    parser = argparse.ArgumentParser(
+        description='RLLIB & SUMO Gallery generator.')
+    parser.add_argument(
+        '--dir-tree', required=True, type=str, 
+        help='Graphs directory.')
+    parser.add_argument(
+        '--exp', required=True, type=str, 
+        help='Experiment name.')
+    parser.add_argument(
+        '--profiler', dest='profiler', action='store_true', help='Enable cProfile.')
+    parser.set_defaults(profiler=False)
+    return parser.parse_args()
+
+def _main():
+    """ Process the graph directory structure generating a static HTML gallery. """
+
+    config = _argument_parser()
+
+    ## ========================              PROFILER              ======================== ##
+    if config.profiler:
+        profiler = cProfile.Profile()
+        profiler.enable()
+    ## ========================              PROFILER              ======================== ##
+
+    gallery = HTMLGallery(config.dir_tree, config.exp)
+    gallery.generate()
+    gallery.save()
+    LOGGER.info('Done')
+
+    ## ========================              PROFILER              ======================== ##
+    if config.profiler:
+        profiler.disable()
+        results = io.StringIO()
+        pstats.Stats(profiler, stream=results).sort_stats('cumulative').print_stats(50)
+        LOGGER.info('Profiler: \n%s', pformat(results.getvalue()))
+    ## ========================              PROFILER              ======================== ##
+
+class HTMLGallery():
+    """ Generate a HTML Gallery. """
+
+    GALLERY_TEMPLATE = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>{{exp_name}}</title>
+    </head>
+    <style>
+        img {
+            border: 1px solid #ddd; /* Gray border */
+            border-radius: 4px;  /* Rounded border */
+            padding: 5px; /* Some padding */
+            width: 300px; /* Set a small width */
+        }
+
+        /* Add a hover effect (blue shadow) */
+        img:hover {
+            box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
+        }
+        </style>
+    <body>
+        <h1>{{exp_name}}</h1>
+
+        <h2>Aggregated data</h2>
+        <table>
+        <tr>
+            <td width="33%"><figure><a target="_blank" href="{{rewards}}"><img src="{{rewards}}"/></a><figcaption>Average reward</figcaption></figure></td>
+            <td width="33%"><figure><a target="_blank" href="{{agents}}"><img src="{{agents}}"/></a><figcaption>Agents overview</figcaption></figure></td>
+            <td width="33%"><figure><a target="_blank" href="{{modes}}"><img src="{{modes}}"/></a><figcaption>Modes overview</figcaption></figure></td>
+        </tr>
+        </table>
+
+        <h2>Agents overview</h2>
+        <table>
+        {% for item in items %}
+        <tr>
+            <td width="25%"><figure><a target="_blank" href="{{item.img1}}"><img src="{{item.img1}}"/></a><figcaption>{{item.caption1}}</figcaption></figure></td>
+            <td width="25%"><figure><a target="_blank" href="{{item.img2}}"><img src="{{item.img2}}"/></a><figcaption>{{item.caption2}}</figcaption></figure></td>
+            <td width="25%"><figure><a target="_blank" href="{{item.img3}}"><img src="{{item.img3}}"/></a><figcaption>{{item.caption3}}</figcaption></figure></td>
+            <td width="25%"><figure><a target="_blank" href="{{item.img4}}"><img src="{{item.img4}}"/></a><figcaption>{{item.caption4}}</figcaption></figure></td>
+        </tr>
+        {% endfor %}
+        </table>
+
+    </body>
+</html>
+    """
+
+    REWARD_SUFFIX = ".aggregated-overview.png"
+    AGENTS_SUFFIX = ".agents-decisions-overview.png"
+    MODES_SUFFIX = ".mode-share-overview.png"
+
+
+    def __init__(self, directory, exp):
+        self.dir = directory
+        self.experiment = exp
+        self.html_template = Template(self.GALLERY_TEMPLATE)
+        self.html_string = None
+
+    @staticmethod
+    def chunks(lst, n):
+        """Yield successive n-sized chunks from lst."""
+        for i in range(0, len(lst), n):
+            yield lst[i:i + n]
+
+    @staticmethod
+    def get_item(lst, pos):
+        return lst[pos] if pos < len(lst) else None
+
+    def generate(self):
+        """ Generates the gallery structure. """
+
+        ## aggregated data
+        rewards = None
+        agents = None
+        modes = None
+        for item in os.listdir(os.path.join(self.dir, 'aggregated')):
+            if self.REWARD_SUFFIX in item:
+                rewards = os.path.join('aggregated', item)
+            elif self.AGENTS_SUFFIX in item:
+                agents = os.path.join('aggregated', item)
+            elif self.MODES_SUFFIX in item:
+                modes = os.path.join('aggregated', item)
+
+        ## agents
+        files = []
+        for item in os.listdir(os.path.join(self.dir, 'agents')):
+            if '.png' in item:
+                files.append(item)
+        items = []
+        for chunk in self.chunks(files, 4):
+            items.append({
+                'img1': os.path.join('agents', self.get_item(chunk, 0)),
+                'caption1': self.get_item(chunk, 0),
+                'img2': os.path.join('agents', self.get_item(chunk, 1)),
+                'caption2': self.get_item(chunk, 1),
+                'img3': os.path.join('agents', self.get_item(chunk, 2)),
+                'caption3': self.get_item(chunk, 2),
+                'img4': os.path.join('agents', self.get_item(chunk, 3)),
+                'caption4': self.get_item(chunk, 3),
+            })
+
+        self.html_string = self.html_template.render(
+            exp_name=self.experiment, rewards=rewards, agents=agents, modes=modes, items=items)
+    
+    def save(self):
+        """ Save gallery to file. """
+        with open(os.path.join(self.dir, 'gallery.html'), 'w') as fstream:
+            fstream.write(self.html_string)
+
+####################################################################################################
+
+if __name__ == '__main__':
+    _main()
+
+####################################################################################################
