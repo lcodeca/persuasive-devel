bfd3179862b4fbd88a61305dd76fc4269d39d521
Lara CODECA <lara.codeca@gmail.com>
2020-05-20 15:26:36 +0100

    QValues insight & gallery

---
+++ src/utils/DBLoggerCustomStats/qvalues.insight.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python3
+
+""" Process the DBLogger directory structure generating Q-values plots for specific episodes. """
+
+import argparse
+from collections import defaultdict
+import cProfile
+import io
+import json
+import logging
+import os
+from pprint import pformat, pprint
+import pstats
+import re
+import sys
+
+from deepdiff import DeepDiff
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+
+from dbloggerstats import DBLoggerStats
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+
+SMALL_SIZE = 20
+MEDIUM_SIZE = SMALL_SIZE + 4
+BIGGER_SIZE = MEDIUM_SIZE + 4
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+def _argument_parser():
+    """ Argument parser for the stats parser. """
+    parser = argparse.ArgumentParser(
+        description='RLLIB & SUMO Statistics parser.')
+    parser.add_argument(
+        '--dir-tree', required=True, type=str, 
+        help='DBLogger directory.')
+    parser.add_argument(
+        '--training', default=None, type=str, 
+        help='Training run to parse, if not defined, it process them all. (Ignored if --last-run)')
+    parser.add_argument(
+        '--agent', default=None, type=str, 
+        help='Agent to parse, if not defined, it process them all. (Ignored if --last-run)')
+    parser.add_argument(
+        '--graph', required=True, 
+        help='Output prefix for the graph(s).')
+    parser.add_argument(
+        '--last-run', dest='last_run', action='store_true', 
+        help='Process all episodes and agents in the last training run.')
+    parser.set_defaults(last_run=False)
+    parser.add_argument(
+        '--profiler', dest='profiler', action='store_true', help='Enable cProfile.')
+    parser.set_defaults(profiler=False)
+    return parser.parse_args()
+
+def _main():
+    """ Process the DBLogger directory structure generating Q-values plots for specific episodes. """
+
+    config = _argument_parser()
+
+    ## ========================              PROFILER              ======================== ##
+    if config.profiler:
+        profiler = cProfile.Profile()
+        profiler.enable()
+    ## ========================              PROFILER              ======================== ##
+
+    statistics = QValuesInsight(
+        config.dir_tree, config.graph, config.training, config.agent, config.last_run)
+    statistics.generate_plots()
+    LOGGER.info('Done')
+
+    ## ========================              PROFILER              ======================== ##
+    if config.profiler:
+        profiler.disable()
+        results = io.StringIO()
+        pstats.Stats(profiler, stream=results).sort_stats('cumulative').print_stats(50)
+        LOGGER.info('Profiler: \n%s', pformat(results.getvalue()))
+    ## ========================              PROFILER              ======================== ##
+
+class QValuesInsight(DBLoggerStats):
+    """ 
+        Process the DBLogger directory structure generating Q-values plots for specific episodes. 
+    """
+
+    def __init__(self, directory, prefix, training, agent, last):
+        super().__init__(directory)
+        self.output_prefix = prefix
+        self.training = training
+        self.agent = agent
+        self.last = last
+        if self.last:
+            self.training, self.agent = None, None
+
+    ######################################## PLOT GENERATOR ########################################
+
+    def generate_plots(self):
+        available_training_runs = list()
+        if self.training:
+            available_training_runs.append(self.training)
+        else:
+            available_training_runs = self.alphanumeric_sort(os.listdir(self.dir))
+        
+        if self.last:
+            available_training_runs = [available_training_runs[-1]]
+
+        print('Processing the training runs...')
+        for training_run in tqdm(available_training_runs):
+            available_agents, _, _ = self.get_training_components(training_run)
+            if self.agent:
+                available_agents = [self.agent]
+            print('Processing agents...')
+            for agent in tqdm(available_agents):
+                qtable = self.get_qtable(training_run, agent)
+                best_action = self.get_best_actions(training_run, agent)
+                state_action_counter = self.get_state_action_counter(training_run, agent)
+                structure = defaultdict(lambda: defaultdict(lambda: list()))
+                for state, action in best_action.items():
+                    if state not in state_action_counter:
+                        # irrelevant
+                        continue
+                    time_left_match = re.search('\(\'time-left\', (.+?)\), \(\'ett\'', state)
+                    if not time_left_match:
+                        continue
+                    time_left = time_left_match.group(1)
+                    ett_match = re.search('array\((.+?)\)\)]\)', state)
+                    if not ett_match:
+                        continue
+                    ett = ett_match.group(1)
+                    counter = state_action_counter[state][str(action)]
+                    if counter > 0:
+                        structure[int(time_left)][int(action)].append('{} ({}) {}'.format(
+                            counter, round(qtable[state][str(action)]), ett))
+
+                x_coord = []
+                y_coord = []
+                labels = []
+                for time_left, values in structure.items():
+                    for action, all_labels in values.items():
+                        aggr = ''
+                        for lbl in all_labels:
+                            aggr += ' {} /'.format(lbl)
+                        aggr = aggr.strip('/')
+                        x_coord.append(time_left)
+                        y_coord.append(action)
+                        labels.append(aggr)
+                
+                ## PLOTTING TIME!
+                fig, ax = plt.subplots(figsize=(60, 30))
+                ax.scatter(x_coord, y_coord, label='Best Action')
+                ax.set_ylim(-0.5, max(y_coord)+0.5)
+                ax.set_yticks(range(0, max(y_coord)+1, 1))
+                for i, txt in enumerate(labels):
+                    ax.annotate(txt, (x_coord[i], y_coord[i]), rotation=90, horizontalalignment='center')
+                ax.set(xlabel='Waiting slots', ylabel='Action [#]', 
+                       title='Best Action "{}" during {}.'.format(agent, training_run))
+                ax.grid()
+                fig.savefig('{}.{}.{}.svg'.format(
+                                self.output_prefix, training_run, agent),
+                            dpi=300, transparent=False, bbox_inches='tight')
+                # fig.savefig('{}.{}.{}.png'.format(
+                #                 self.output_prefix, training_run, agent),
+                #             dpi=300, transparent=False, bbox_inches='tight')
+                # plt.show()   
+                matplotlib.pyplot.close('all')
+
+####################################################################################################
+
+if __name__ == '__main__':
+    _main()
+
+####################################################################################################
