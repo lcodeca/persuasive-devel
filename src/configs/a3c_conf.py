b6e3701b19ab49f62c1fe908c6768c1870dfeef9
Lara CODECA <lara.codeca@gmail.com>
2020-08-17 10:43:42 +0100

    Custom A3C, LSTM and updates for RLLIB 0.8.6

--- src/configs/a3c_conf.py
+++ src/configs/a3c_conf.py
@@ -4,18 +4,19 @@
 
 from pprint import pprint
 
-from learning.persuasivea3ctrainer import DEFAULT_CONFIG
-# from ray.rllib.models import ModelCatalog
-# from ray.rllib.examples.models.centralized_critic_models import CentralizedCriticModel
+from learning.persuasivea3c import DEFAULT_CONFIG
+
+from ray.rllib.models import ModelCatalog
+from learning.lstm import RNNModel
 
 def persuasive_a3c_conf(tr_steps=1, debug_folder=None, alpha=0.0001, gamma=0.99):
     """
-        https://github.com/ray-project/ray/blob/releases/0.8.6/rllib/agents/trainer.py#L44
-        https://github.com/ray-project/ray/blob/releases/0.8.6/rllib/agents/a3c/a3c.py#L14
-        https://github.com/ray-project/ray/blob/releases/0.8.6/rllib/models/catalog.py#L37
+        https://github.com/ray-project/ray/blob/releases/0.8.7/rllib/agents/trainer.py#L44
+        https://github.com/ray-project/ray/blob/releases/0.8.7/rllib/agents/a3c/a3c.py#L14
+        https://github.com/ray-project/ray/blob/releases/0.8.7/rllib/models/catalog.py#L37
     """
 
-    # ModelCatalog.register_custom_model('custom_ccm', CentralizedCriticModel)
+    ModelCatalog.register_custom_model('custom_rrn', RNNModel)
 
     custom_configuration = DEFAULT_CONFIG
 
@@ -36,20 +37,34 @@ def persuasive_a3c_conf(tr_steps=1, debug_folder=None, alpha=0.0001, gamma=0.99)
     custom_configuration['remote_env_batch_wait_ms'] = 1000
     custom_configuration['remote_worker_envs'] = False
     custom_configuration['seed'] = 42
-    custom_configuration['timesteps_per_iteration'] = 1000
+    custom_configuration['timesteps_per_iteration'] = 1
     custom_configuration['train_batch_size'] = tr_steps
 
+    # === Exploration Settings ===
+    # Default exploration behavior, iff `explore`=None is passed into
+    # compute_action(s).
+    # Set to False for no exploration behavior (e.g., for evaluation).
+    custom_configuration['explore'] = True
+    # Provide a dict specifying the Exploration object's config.
+    # The Exploration class to use. In the simplest case, this is the name
+    # (str) of any class present in the `rllib.utils.exploration` package.
+    # You can also provide the python class directly or the full location
+    # of your class (e.g. "ray.rllib.utils.exploration.epsilon_greedy.
+    # EpsilonGreedy").
+    custom_configuration['exploration_config']['type'] = 'StochasticSampling'
+    # Add constructor kwargs here (if any).
+
     # == MODEL - DEFAULT ==
-    custom_configuration['model']['fcnet_hiddens'] = [64, 64]
+    # custom_configuration['model']['fcnet_hiddens'] = [64, 64]
 
     # == MODEL - LSTM ==
-    custom_configuration['model']['lstm_cell_size'] = 64
-    custom_configuration['model']['max_seq_len'] = 2
-    custom_configuration['model']['state_shape'] = [64, 64]
-    custom_configuration['model']['use_lstm'] = True
+    # custom_configuration['model']['lstm_cell_size'] = 64
+    # custom_configuration['model']['max_seq_len'] = 2
+    # custom_configuration['model']['state_shape'] = [64, 64]
+    # custom_configuration['model']['use_lstm'] = True
 
     # == MODEL - CUSTOM ==
-    # custom_configuration['model']['custom_model'] = 'custom_ccm'
+    custom_configuration['model']['custom_model'] = 'custom_rrn'
 
     # == Persuasive A3C ==
     custom_configuration['lr'] = alpha
