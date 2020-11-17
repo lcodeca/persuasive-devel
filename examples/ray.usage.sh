53dbb38f5ada62e6ee4e0e3310bda3f995ac57f0
Lara CODECA <lara.codeca@gmail.com>
2020-11-17 10:49:17 +0000

    example of ray usage

--- examples/ray.usage.sh
+++ examples/ray.usage.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
 #### ALICE ####
-ray start --head --port 6379 --num-cpus 7 --num-gpus 0 --code-search-path=/home/alice/devel/persuasive-devel/
+ray start --head --num-cpus 7 --num-gpus 0 --code-search-path=/home/alice/devel/persuasive-devel/
 
 #### FASTIDIO ####
-ray start --address='192.168.1.25:6379' --redis-password='5241590000000000' --num-cpus 11 --num-gpus 1 --code-search-path=/home/alice/devel/persuasive-devel/
+ray start --address='192.168.1.25:6379' --redis-password='5241590000000000' --num-cpus 1 --num-gpus 1 --code-search-path=/home/alice/devel/persuasive-devel/
+
+#### Both ####
+htop -s PERCENT_MEM
