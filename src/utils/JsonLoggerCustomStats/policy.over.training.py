a7ca63b24fbb082a8cfe5a3eec3a2b6783e571dd
Lara CODECA <lara.codeca@gmail.com>
2020-11-17 10:51:45 +0000

    new policy graphs

--- src/utils/JsonLoggerCustomStats/policy.over.training.py
+++ src/utils/JsonLoggerCustomStats/policy.over.training.py
@@ -57,7 +57,10 @@ def _argument_parser():
     """ Argument parser for the stats parser. """
     parser = argparse.ArgumentParser(description='RLLIB & SUMO Statistics parser.')
     parser.add_argument('--results', required=True, type=str, help='RESULTS, JSON file.')
-    parser.add_argument('--init', required=True, type=str, help='AGENT INIT, JSON file.')
+    parser.add_argument('--init-learn', required=True, type=str,
+                        help='AGENT INIT during learning, JSON file.')
+    parser.add_argument('--init-eval', required=True, type=str,
+                        help='AGENT INIT during evaluation, JSON file.')
     parser.add_argument('--prefix', default='stats', help='Output prefix for the processed data.')
     parser.add_argument('--max', default=None, type=int, help='Maximum number of runs.')
     parser.add_argument('--profiler', dest='profiler', action='store_true', help='Enable cProfile.')
@@ -67,9 +70,11 @@ def _argument_parser():
 class Policy(object):
     """ Process a SINGLE RLLIB logs/result.json file as a time series. """
 
-    def __init__(self, agents, results, prefix, max_runs):
-        with open(agents, 'r') as jsonfile:
-            self.agents_init = json.load(jsonfile)
+    def __init__(self, agents_learn, agents_eval, results, prefix, max_runs):
+        with open(agents_learn, 'r') as jsonfile:
+            self.agents_init_learning = json.load(jsonfile)
+        with open(agents_eval, 'r') as jsonfile:
+            self.agents_init_evaluation = json.load(jsonfile)
         self.max_runs = max_runs
         self.results_file = results
         self.prefix = prefix
@@ -94,16 +99,11 @@ class Policy(object):
                 print(value, key)
                 input('Press any key...')
 
+    def _perc(self, num):
+        return round(num/len(self.agents_init_learning)*100.0, 1)
+
     def historical_policies(self):
-        counter = 0
-        labels = [
-            # mpatches.Patch(color='black', label='Wait'),
-            mpatches.Patch(color='red', label='Car'),
-            mpatches.Patch(color='green', label='PT'),
-            mpatches.Patch(color='orange', label='Walk'),
-            mpatches.Patch(color='blue', label='Bicycle'),
-            mpatches.Patch(color='purple', label='PTW'),
-        ]
+        counter = 1
 
         modes_color = {
             0: 'black',
@@ -129,43 +129,91 @@ class Policy(object):
                 complete = json.loads(row)
 
                 ############ LEARNIN
-                axs[0].bar(9.0, len(self.agents_init), width=0.05, color='r', align='center')
+                axs[0].bar(9.0, len(self.agents_init_learning),
+                           width=0.05, color='r', align='center')
+                axs[0].bar(8.75, len(self.agents_init_learning),
+                           width=0.02, color='g', align='center')
                 axs[0].set_xlim(6.0, 10.0)
-                axs[0].set_ylim(-1, len(self.agents_init))
-                axs[0].set_title('Learning')
+                axs[0].set_ylim(-1, len(self.agents_init_learning))
                 axs[0].set_xlabel('Time [h]')
                 axs[0].set_ylabel('Agents [#]')
-                axs[0].legend(handles=labels, loc='upper left')
 
                 info_by_episode = complete['hist_stats']['info_by_agent']
                 last_action_by_agent = complete['hist_stats']['last_action_by_agent']
                 # rewards_by_agent = complete['hist_stats']['rewards_by_agent']
                 pos = random.randrange(len(info_by_episode))
                 episode = info_by_episode[pos]
+
+                mode_usage = collections.defaultdict(int)
+                on_time = 0
+                too_late = 0
+                too_early = 0
+                missing = 0
                 for agent, info in episode.items():
                     try:
                         y_agent = int(agent.split('_')[1])
-                        start = self.agents_init[agent]['start']/3600
+                        start = self.agents_init_learning[agent]['start']/3600
                         mode = last_action_by_agent[pos][agent]
                         arrival = info['arrival']/3600.0
                         departure = info['departure']/3600.0
 
                         y = [y_agent, y_agent]
-                        axs[0].plot([start, departure], y, color='black', alpha=0.75)
-                        axs[0].plot([departure, arrival], y, color=modes_color[mode], alpha=0.75)
+                        axs[0].plot([start, departure], y, color='black', alpha=0.5)
+                        axs[0].plot([departure, arrival], y, color=modes_color[mode], alpha=0.9)
+
+                        # STATS
+                        mode_usage[mode] += 1
+                        if np.isnan(arrival):
+                            missing += 1
+                        else:
+                            if info['arrival'] > 9 * 3600:
+                                too_late += 1
+                            elif info['arrival'] > (9 * 3600 - 15 * 60):
+                                on_time += 1
+                            else:
+                                too_early += 1
+
                     except KeyError as exception:
                         print(exception)
                         logger.critical('[Learning] Missing stats in %d', counter)
+                title = 'Learning \n'
+                title += 'early {} - on time {} - late {} - missing {} \n'.format(
+                    too_early, on_time, too_late, missing)
+                title += 'early {}% - on time {}% - late {}% - missing {}% \n'.format(
+                    self._perc(too_early), self._perc(on_time), self._perc(too_late),
+                    self._perc(missing))
+                axs[0].set_title(title)
+                labels = [
+                    mpatches.Patch(
+                        color='black', label='Wait \n({}%)'.format(self._perc(mode_usage[0]))),
+                    mpatches.Patch(
+                        color='red', label='Car \n({}%)'.format(self._perc(mode_usage[1]))),
+                    mpatches.Patch(
+                        color='green', label='PT \n({}%)'.format(self._perc(mode_usage[2]))),
+                    mpatches.Patch(
+                        color='orange', label='Walk \n({}%)'.format(self._perc(mode_usage[3]))),
+                    mpatches.Patch(
+                        color='blue', label='Bicycle \n({}%)'.format(self._perc(mode_usage[4]))),
+                    mpatches.Patch(
+                        color='purple', label='PTW \n({}%)'.format(self._perc(mode_usage[5]))),
+                ]
+                axs[0].legend(handles=labels, loc='upper left')
 
                 ############ EVALUATION
-                axs[1].bar(9.0, len(self.agents_init), width=0.05, color='r', align='center')
+                axs[1].bar(9.0, len(self.agents_init_evaluation),
+                           width=0.05, color='r', align='center')
+                axs[1].bar(8.75, len(self.agents_init_learning),
+                           width=0.02, color='g', align='center')
                 axs[1].set_xlim(6.0, 10.0)
-                axs[1].set_ylim(-1, len(self.agents_init))
-                axs[1].set_title('Evaluation')
+                axs[1].set_ylim(-1, len(self.agents_init_evaluation))
                 axs[1].set_xlabel('Time [h]')
                 axs[1].set_ylabel('Agents [#]')
-                axs[1].legend(handles=labels, loc='upper left')
 
+                mode_usage = collections.defaultdict(int)
+                on_time = 0
+                too_late = 0
+                too_early = 0
+                missing = 0
                 if 'evaluation' in complete:
                     complete = complete['evaluation']
                     info_by_episode = complete['hist_stats']['info_by_agent']
@@ -176,17 +224,54 @@ class Policy(object):
                     for agent, info in episode.items():
                         try:
                             y_agent = int(agent.split('_')[1])
-                            start = self.agents_init[agent]['start']/3600
+                            start = self.agents_init_evaluation[agent]['start']/3600
                             mode = last_action_by_agent[pos][agent]
                             arrival = info['arrival']/3600.0
                             departure = info['departure']/3600.0
 
                             y = [y_agent, y_agent]
-                            axs[1].plot([start, departure], y, color='black', alpha=0.99)
-                            axs[1].plot([departure, arrival], y, color=modes_color[mode], alpha=0.99)
+                            axs[1].plot([start, departure], y, color='black', alpha=0.5)
+                            axs[1].plot([departure, arrival], y,
+                                        color=modes_color[mode], alpha=0.9)
+
+                            # STATS
+                            mode_usage[mode] += 1
+                            if np.isnan(arrival):
+                                missing += 1
+                            else:
+                                if info['arrival'] > 9 * 3600:
+                                    too_late += 1
+                                elif info['arrival'] > (9 * 3600 - 15 * 60):
+                                    on_time += 1
+                                else:
+                                    too_early += 1
+
                         except KeyError as exception:
                             print(exception)
                             logger.critical('[Evaluation] Missing stats in %d', counter)
+
+                title = 'Evaluation \n'
+                title += 'early {} - on time {} - late {} - missing {} \n'.format(
+                    too_early, on_time, too_late, missing)
+                title += 'early {}% - on time {}% - late {}% - missing {}% \n'.format(
+                    self._perc(too_early), self._perc(on_time), self._perc(too_late),
+                    self._perc(missing))
+                axs[1].set_title(title)
+                labels = [
+                    mpatches.Patch(
+                        color='black', label='Wait\n({}%)'.format(self._perc(mode_usage[0]))),
+                    mpatches.Patch(
+                        color='red', label='Car \n({}%)'.format(self._perc(mode_usage[1]))),
+                    mpatches.Patch(
+                        color='green', label='PT \n({}%)'.format(self._perc(mode_usage[2]))),
+                    mpatches.Patch(
+                        color='orange', label='Walk \n({}%)'.format(self._perc(mode_usage[3]))),
+                    mpatches.Patch(
+                        color='blue', label='Bicycle \n({}%)'.format(self._perc(mode_usage[4]))),
+                    mpatches.Patch(
+                        color='purple', label='PTW \n({}%)'.format(self._perc(mode_usage[5]))),
+                ]
+                axs[1].legend(handles=labels, loc='upper left')
 
                 ################################################################
                 fig.savefig('{}.policy.{}.svg'.format(self.prefix, counter),
@@ -215,7 +300,8 @@ def _main():
         profiler.enable()
     ## ========================              PROFILER              ======================== ##
 
-    statistics = Policy(config.init, config.results, config.prefix, config.max)
+    statistics = Policy(
+        config.init_learn, config.init_eval, config.results, config.prefix, config.max)
     statistics.historical_policies()
     logging.info('Done')
 
@@ -225,6 +311,7 @@ def _main():
         results = io.StringIO()
         pstats.Stats(profiler, stream=results).sort_stats('cumulative').print_stats(50)
         logging.info('Profiler: \n%s', pformat(results.getvalue()))
+        print('Profiler: \n%s', results.getvalue())
     ## ========================              PROFILER              ======================== ##
 
 if __name__ == '__main__':
