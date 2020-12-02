b8e791d46965cd84c4929e3fcd094fa6b16ee301
Lara CODECA <lara.codeca@gmail.com>
2020-12-02 17:36:28 +0000

    incremental arrival over learning

--- src/utils/CustomMetrics/stat.arrival.over.learning.py
+++ src/utils/CustomMetrics/stat.arrival.over.learning.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-""" Process the RLLIB  metrics_XYZ.json """
+""" Process the RLLIB metrics_XYZ.json """
 
 import argparse
 import collections
@@ -67,7 +67,7 @@ def _main():
         profiler.enable()
     ## ========================              PROFILER              ======================== ##
 
-    MissingAgents(config.input_dir, config.output_dir).generate()
+    OnTimeAgents(config.input_dir, config.output_dir).generate()
     logging.info('Done')
 
     ## ========================              PROFILER              ======================== ##
@@ -80,26 +80,30 @@ def _main():
 
 ####################################################################################################
 
-class MissingAgents(GenericGraphMaker):
+class OnTimeAgents(GenericGraphMaker):
 
     def __init__(self, input_dir, output_dir):
         _default = {
             'learning': {
                 'timesteps_total': [],
-                'episode_missing_agents_min': [],
-                'episode_missing_agents_mean': [],
-                'episode_missing_agents_max':[],
+                'arrival_min': [],
+                'arrival_mean': [],
+                'arrival_median': [],
+                'arrival_std': [],
+                'arrival_max':[],
             },
             'evaluation': {
                 'timesteps_total': [],
-                'episode_missing_agents_min': [],
-                'episode_missing_agents_mean': [],
-                'episode_missing_agents_max':[],
+                'arrival_min': [],
+                'arrival_mean': [],
+                'arrival_median': [],
+                'arrival_std': [],
+                'arrival_max':[],
             },
         }
         super().__init__(
             input_dir, output_dir,
-            filename='missing-agents.json',
+            filename='arrival.json',
             default=_default)
 
     def _find_last_metric(self):
@@ -114,78 +118,107 @@ class MissingAgents(GenericGraphMaker):
                 # LEARNING
                 self._aggregated_dataset['learning']['timesteps_total'].append(
                     complete['timesteps_total'])
-                self._aggregated_dataset['learning']['episode_missing_agents_min'].append(
-                    complete['custom_metrics']['episode_missing_agents_min'])
-                self._aggregated_dataset['learning']['episode_missing_agents_mean'].append(
-                    complete['custom_metrics']['episode_missing_agents_mean'])
-                self._aggregated_dataset['learning']['episode_missing_agents_max'].append(
-                    complete['custom_metrics']['episode_missing_agents_max'])
+                arrivals = []
+                for episode in complete['hist_stats']['info_by_agent']:
+                    for info in episode.values():
+                        arrivals.append(info['arrival']/3600)
+                self._aggregated_dataset['learning']['arrival_min'].append(
+                    np.nanmin(arrivals))
+                self._aggregated_dataset['learning']['arrival_median'].append(
+                    np.nanmedian(arrivals))
+                self._aggregated_dataset['learning']['arrival_mean'].append(
+                    np.nanmean(arrivals))
+                self._aggregated_dataset['learning']['arrival_std'].append(
+                    np.nanstd(arrivals))
+                self._aggregated_dataset['learning']['arrival_max'].append(
+                    np.nanmax(arrivals))
 
                 # EVALUATION
                 if 'evaluation' in complete:
                     complete['evaluation']['timesteps_total'] = complete['timesteps_total']
                     complete = complete['evaluation']
 
+                    arrivals = []
+                    for episode in complete['hist_stats']['info_by_agent']:
+                        for info in episode.values():
+                            arrivals.append(info['arrival']/3600)
+
                     self._aggregated_dataset['evaluation']['timesteps_total'].append(
                         complete['timesteps_total'])
-                    self._aggregated_dataset['evaluation']['episode_missing_agents_min'].append(
-                        complete['custom_metrics']['episode_missing_agents_min'])
-                    self._aggregated_dataset['evaluation']['episode_missing_agents_mean'].append(
-                        complete['custom_metrics']['episode_missing_agents_mean'])
-                    self._aggregated_dataset['evaluation']['episode_missing_agents_max'].append(
-                        complete['custom_metrics']['episode_missing_agents_max'])
+                    self._aggregated_dataset['evaluation']['arrival_min'].append(
+                        np.nanmin(arrivals))
+                    self._aggregated_dataset['evaluation']['arrival_median'].append(
+                        np.nanmedian(arrivals))
+                    self._aggregated_dataset['evaluation']['arrival_mean'].append(
+                        np.nanmean(arrivals))
+                    self._aggregated_dataset['evaluation']['arrival_std'].append(
+                        np.nanstd(arrivals))
+                    self._aggregated_dataset['evaluation']['arrival_max'].append(
+                        np.nanmax(arrivals))
 
     def _generate_graphs(self):
         # LEARNING
         fig, ax = plt.subplots(figsize=(15, 10))
+        ax.errorbar(
+            self._aggregated_dataset['learning']['timesteps_total'],
+            self._aggregated_dataset['learning']['arrival_mean'],
+            yerr=self._aggregated_dataset['learning']['arrival_std'],
+            capsize=5, label='Mean [std]', fmt='-o')
         ax.plot(
             self._aggregated_dataset['learning']['timesteps_total'],
-            self._aggregated_dataset['learning']['episode_missing_agents_mean'],
-            '-o', label='Mean')
+            self._aggregated_dataset['learning']['arrival_min'], label='Min')
         ax.plot(
             self._aggregated_dataset['learning']['timesteps_total'],
-            self._aggregated_dataset['learning']['episode_missing_agents_min'], label='Min')
+            self._aggregated_dataset['learning']['arrival_max'], label='Max')
         ax.plot(
             self._aggregated_dataset['learning']['timesteps_total'],
-            self._aggregated_dataset['learning']['episode_missing_agents_max'], label='Max')
-        ax.set(xlabel='Learning step', ylabel='Missing Agents',
-               title='[L] Missing Agents Over Time')
+            self._aggregated_dataset['learning']['arrival_median'], label='Median')
+        ax.set(
+            xlabel='Learning step', ylabel='Time [h]',
+            title='[L] Arrival at destination over time.')
         ax.legend(loc='best', ncol=4, shadow=True)
         ax.grid()
-        fig.savefig('{}/learning.missing_agents_over_learning.svg'.format(self._output_dir),
+        fig.savefig('{}/learning.average_arrival_over_learning.svg'.format(self._output_dir),
                     dpi=300, transparent=False, bbox_inches='tight')
         #plt.show()
         matplotlib.pyplot.close('all')
 
         # EVALUATION
         fig, ax = plt.subplots(figsize=(15, 10))
+        ax.errorbar(
+            self._aggregated_dataset['evaluation']['timesteps_total'],
+            self._aggregated_dataset['evaluation']['arrival_mean'],
+            yerr=self._aggregated_dataset['evaluation']['arrival_std'],
+            capsize=5, label='Mean [std]', fmt='-o')
         ax.plot(
             self._aggregated_dataset['evaluation']['timesteps_total'],
-            self._aggregated_dataset['evaluation']['episode_missing_agents_mean'],
-            '-o', label='Mean')
+            self._aggregated_dataset['evaluation']['arrival_min'], label='Min')
         ax.plot(
             self._aggregated_dataset['evaluation']['timesteps_total'],
-            self._aggregated_dataset['evaluation']['episode_missing_agents_min'], label='Min')
+            self._aggregated_dataset['evaluation']['arrival_max'], label='Max')
         ax.plot(
             self._aggregated_dataset['evaluation']['timesteps_total'],
-            self._aggregated_dataset['evaluation']['episode_missing_agents_max'], label='Max')
-        ax.set(xlabel='Learning step', ylabel='Missing Agents',
-               title='[E] Missing Agents Over Time')
+            self._aggregated_dataset['evaluation']['arrival_median'], label='Median')
+        ax.set(
+            xlabel='Learning step', ylabel='Time [h]',
+            title='[L] Arrival at destination over time.')
         ax.legend(loc='best', ncol=4, shadow=True)
         ax.grid()
-        fig.savefig('{}/evaluation.missing_agents_over_learning.svg'.format(self._output_dir),
+        fig.savefig('{}/evaluation.average_arrival_over_learning.svg'.format(self._output_dir),
                     dpi=300, transparent=False, bbox_inches='tight')
         #plt.show()
         matplotlib.pyplot.close('all')
 
 ####################################################################################################
 
-    # def missing_agents_over_timesteps_total(self):
-    #     logger.info('Computing the missing agents over the timesteps total.')
+    # def average_arrival_over_timesteps_total(self):
+    #     logger.info('Computing the average arrival time over the timesteps total.')
     #     x_coords = []
     #     y_coords = []
+    #     median_y = []
     #     min_y = []
     #     max_y = []
+    #     std_y = []
     #     with open(self.input, 'r') as jsonfile:
     #         counter = 0
     #         for row in tqdm(jsonfile): # enumerate cannot be used due to the size of the file
@@ -198,21 +231,30 @@ class MissingAgents(GenericGraphMaker):
     #                 else:
     #                     # evaluation stats requested but not present in the results
     #                     continue
-
-    #             x_coords.append(complete['timesteps_total'])
-    #             y_coords.append(complete['custom_metrics']['episode_missing_agents_mean'])
-    #             min_y.append(complete['custom_metrics']['episode_missing_agents_min'])
-    #             max_y.append(complete['custom_metrics']['episode_missing_agents_max'])
+    #             if 'info_by_agent' in complete['hist_stats']:
+    #                 x_coords.append(complete['timesteps_total'])
+    #                 arrivals = []
+    #                 for episode in complete['hist_stats']['info_by_agent']:
+    #                     for info in episode.values():
+    #                         arrivals.append(info['arrival']/3600)
+    #                 y_coords.append(np.nanmean(arrivals))
+    #                 min_y.append(np.nanmin(arrivals))
+    #                 max_y.append(np.nanmax(arrivals))
+    #                 median_y.append(np.nanmedian(arrivals))
+    #                 std_y.append(np.nanstd(arrivals))
+    #             else:
+    #                 logger.critical('Missing stats in row %d', counter)
     #             counter += 1
 
     #     fig, ax = plt.subplots(figsize=(15, 10))
-    #     ax.errorbar(x_coords, y_coords, capsize=5, label='Mean', fmt='-o')
+    #     ax.errorbar(x_coords, y_coords, yerr=std_y, capsize=5, label='Mean [std]', fmt='-o')
     #     ax.plot(x_coords, min_y, label='Min')
     #     ax.plot(x_coords, max_y, label='Max')
-    #     ax.set(xlabel='Learning step', ylabel='Missing Agents', title='Missing Agents Over Time')
+    #     ax.plot(x_coords, median_y, label='Median')
+    #     ax.set(xlabel='Learning step', ylabel='Time [h]', title='Arrival at destination over time.')
     #     ax.legend(loc='best', ncol=4, shadow=True)
     #     ax.grid()
-    #     fig.savefig('{}.missing_agents_over_learning.svg'.format(self.prefix),
+    #     fig.savefig('{}.average_arrival_over_learning.svg'.format(self.prefix),
     #                 dpi=300, transparent=False, bbox_inches='tight')
     #     #plt.show()
     #     matplotlib.pyplot.close('all')
