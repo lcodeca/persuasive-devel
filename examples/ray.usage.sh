bef3e478425068ff9a3c4905b2507302f82f9fbc
Lara CODECA <lara.codeca@gmail.com>
2021-01-27 12:49:20 +0000

    hopper configs

--- examples/ray.usage.sh
+++ examples/ray.usage.sh
@@ -8,3 +8,9 @@ ray start --address='192.168.1.25:6379' --redis-password='5241590000000000' --nu
 
 #### Both ####
 htop -s PERCENT_MEM
+
+#### HOPPER - Ray master #### #64424509440 --port 0
+ray start --head --num-cpus 20 --num-gpus 1 --code-search-path=/home/alice/devel/persuasive-devel/ --object-store-memory 64324509440
+
+#### HOPPER - Others
+ray start --address='172.17.0.2:6379' --redis-password='5241590000000000' --num-cpus 0 --num-gpus 0 --object-store-memory 10637418240
